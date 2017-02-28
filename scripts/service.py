9e9e845112ef7ddae38162d1e48f9e227d79e9a2
DTobii <tobi00@arcor.de>
2017-02-28 18:58:56 +0000

    added parts of the REST API, fixed part of global variables

--- scripts/service.py
+++ scripts/service.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 import udp
 import tcp
+import time
 from threading import Thread
 import subprocess
+from flask import Flask
+app = Flask(__name__)
 
-class myClassA(Thread):
+class udpClass(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.daemon = True
@@ -12,7 +15,7 @@ class myClassA(Thread):
     def run(self):
         udp.startServer('192.168.1.1',8888,1024)
 
-class myClassB(Thread):
+class tcpClass(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.daemon = True
@@ -20,8 +23,53 @@ class myClassB(Thread):
     def run(self):
         tcp.startServer('192.168.1.1',5005,1024)  
 
+class myApp(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
+    def run(self):
+        app.run(port=8080)
+
+global cudp
+global ctcp
+cudp = udpClass()
+ctcp = tcpClass()
+
+@app.route('/api/server/udp/start')
+def udpstartrest():
+    global cudp
+    global ctcp
+    if cudp.is_alive() == False:
+        cudp = udpClass()
+        print("test %r" %cudp.is_alive())
+    server_status = '{"udp":%r,"tcp":%r}' %(cudp.is_alive(), ctcp.is_alive())
+    res = app.response_class(server_status)
+    res.headers["Content-Type"] = "application/json"
+    return res
+
+@app.route('/api/server/status')
+def statusrest():
+    global ctcp
+    global cudp
+    server_status = '{"udp":%r,"tcp":%r}' % (cudp.is_alive(), ctcp.is_alive())
+    res = app.response_class(server_status)
+    res.headers["Content-Type"] = "application/json"
+    return res
+
+@app.route('/api/server/tcp/start')
+def tcpstartrest():
+    global cudp
+    global ctcp
+    if ctcp.is_alive()==False:
+        ctcp = tcpClass()
+    server_status = '{"udp":%r,"tcp":%r}' % (cudp.is_alive(), ctcp.is_alive())
+    res = app.response_class(server_status)
+    res.headers["Content-Type"] = "application/json"
+    return res
+
+
 if __name__== '__main__':
-    myClassA()
-    myClassB() 
+    appthread = myApp()
     while True:
         pass
